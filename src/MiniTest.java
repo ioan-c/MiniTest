20991f6ee8c620e7c822e8b125ad6470b8dd7369
Ioan Conache <ionutz.conache@hotmail.com>
2019-05-30 23:11:12 +0300

    minor changes

--- src/MiniTest.java
+++ src/MiniTest.java
@@ -1,18 +1,19 @@
 import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 class MiniTest {
 
     static void runTest(){
-        boolean ok = false; //used to make sure the user only inserts numerical values
+        boolean ok = false; //used to make sure the user only inserts numerical values or does not exceed 1000 tests
         do {
             try{
                 testNumbers();
                 ok = true;
             } catch (InputMismatchException ex){
                 ex.printStackTrace();
-                System.out.println("Please enter only enter values of type integer or long!");
+                System.out.println("Please only enter values of type integer or long!");
             } catch (RuntimeException ex) {
                 ex.printStackTrace();
             }
@@ -23,17 +24,19 @@ class MiniTest {
         Scanner systemIn = new Scanner(System.in);
 
         System.out.println("Input:");
-        int T = systemIn.nextInt(); //number of tests
+        int T = systemIn.nextInt(); //input for number of tests
         if(T>1000){
             throw new RuntimeException("The number of tests cannot be over 1000!");
         }
 
-        ArrayList<Long> numbersList = new ArrayList<>();
+        //populating list with data
+        List<Long> numbersList = new ArrayList<>();
         for(int i=0; i<T; i++){
             Long N = systemIn.nextLong();
             numbersList.add(N);
         }
 
+        //testing the numbers in list
         System.out.println("Output:");
         for(Long number : numbersList){
             if(number%4 == 0){
